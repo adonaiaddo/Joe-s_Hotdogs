6e2265b3e5f90c0b94d2eaf576d1b0e8ec4dbaaa
adonaiaddo <76479542+adonaiaddo@users.noreply.github.com>
2022-02-26 11:16:44 -0500

    Add files via upload

---
+++ Joe's_Hotdogs_javascript.js
@@ -0,0 +1,21 @@
+var hotdogs = document.prompt("How many hotdogs do you want?")
+var fries = document.prompt("How many fries do you want?")
+var sodas = document.prompt("How many sodas do you want?")
+
+var subtotal = hotdogs * 3.75 + fries * 2.00 + sodas * 1.80
+
+var discount = 0.00
+if (subtotal >= 20) {
+    discount = subtotal * 0.10
+}
+
+var tax = subtotal / 16
+var final = subtotal + tax
+
+document.write(hotdogs + " hotdogs      " + hotdogs * 3.75 + "\n")
+document.write(fries   + " fries      "   + fries * 2.00   + "\n")
+document.write(sodas   + " sodas      "   + sodas * 1.80   + "\n")
+document.write("subtotal        "         + subtotal       + "\n")
+document.write("discount        "         + discount       + "\n")
+document.write("tax         "             + tax            + "\n")
+document.write("total       " + final)
